9a3171300f8f45402f4581a6c70fdfa159be6c52
tparris4 <45776810+tparris4@users.noreply.github.com>
2019-05-12 15:12:23 -0400

    Add files via upload

---
+++ app/src/main/java/com/theincgi/gles_game_fixed/musicButton.java
@@ -0,0 +1,26 @@
+package com.theincgi.gles_game_fixed;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.theincgi.gles_game_fixed.R.layout.activity_music_button;
+
+public class musicButton extends AppCompatActivity {
+MediaPlayer mySong;
+private Button button;
+private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_music_button);
+        mySong=MediaPlayer.create(this,R.raw.coin_grab_3);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.levelButt);
+    }
+    public void playIT(View view) {
+        mySong.start();
+    }
+}
