3adbc91cbe60ee6f1bba1fc437ce00b5724fb6de
tparris4 <45776810+tparris4@users.noreply.github.com>
2019-05-13 00:20:18 -0400

    Music should now start on this screen

--- app/src/main/java/com/theincgi/gles_game_fixed/musicButton.java
+++ app/src/main/java/com/theincgi/gles_game_fixed/musicButton.java
@@ -1,7 +1,11 @@
 package com.theincgi.gles_game_fixed;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.media.MediaPlayer;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +17,36 @@ private Button playButton;
 private Button selectLevelButton;
 private Button highScoreButton;
 private Button goBack;
+    //music player
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +55,10 @@ private Button goBack;
         playButton = findViewById(R.id.button);
         selectLevelButton = findViewById(R.id.levelButt);
         highScoreButton = findViewById(R.id.highScoreButton);
+        
+        this.doBindService();
+        Intent music = new Intent(this,MusicService.class);
+        startService(music);
     }
     public void playIT(View view) {
         mySong.start();
